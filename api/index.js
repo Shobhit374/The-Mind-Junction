d58b1c4e72003a973ada2736fe325749357ce626
Shobhit Kumar <shobhitkumar43@gmail.com>
2023-05-16 23:20:41 +0530

    Created REST API
    Connected to Mongo DB
    Created functionality for register, login, posts etc.

--- api/index.js
+++ api/index.js
@@ -3,6 +3,10 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
+const usersRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -12,7 +16,24 @@ mongoose.connect(process.env.MONGO_URL,{
     useUnifiedTopology: true
 }).then(console.log("mongo")).catch((err)=>console.log(err));
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth",authRoute);
+app.use("/api/users",usersRoute);
+app.use("/api/posts",postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen("5000", () =>{
     console.log("OK backend");
